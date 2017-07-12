a13a055ce03b58dff0f630fb21e1c0116164cbdb
Fernanda Montanari <fmsantiago@fitec.org.br>
2017-07-12 09:38:19 -0300

    Parking lot app with wcf service layer
    - Presentation project with the interface to checkin and out car by the plate
    - Business project with the logic related to the checkin and out of cars
    - Service project with the services needed, chickin, checkout and consulting
      available parking spots

---
+++ Estacionamento.Service/EstacionamentoService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Estacionamento.Service
+{
+    public class EstacionamentoService : IEstacionamentoService
+    {
+        public double Checkout(string placa)
+        {
+            return Estacionamento.Negocio.Operacoes.Checkout(placa);
+        }
+
+        public void Checkin(string placa)
+        {
+            Estacionamento.Negocio.Operacoes.Checkin(placa);
+        }
+
+        public int VagasRestantes()
+        {
+            return Estacionamento.Negocio.Operacoes.VAGAS_TOTAIS - Estacionamento.Negocio.Operacoes._estacionamento.Count();
+        }
+        
+    }
+}
